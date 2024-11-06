3a5f213325459664492c677306bfbe5dd11ea202
Sharon Xiang <shxi3448@colorado.edu>
2024-11-06 10:49:46 -0700

    Added some SQL tables

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,4 +1,36 @@
-CREATE TABLE users (
+-- DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(50) PRIMARY KEY,
-  password CHAR(60) NOT NULL
+  password CHAR(60) NOT NULL,
+  email VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS recipes (
+  recipe_id SERIAL PRIMARY KEY NOT NULL,
+  recipe_name VARCHAR(75), 
+  recipe_difficulty VARCHAR(100) CONSTRAINT limited_values CHECK (difficulty in ('easy', 'moderate', 'difficult', 'very_difficult'))
+);
+
+CREATE TABLE IF NOT EXISTS images (
+  image_id SERIAL PRIMARY KEY NOT NULL,
+  image_url VARCHAR(300) NOT NULL,
+  image_caption VARCHAR(200)
+);
+
+CREATE TABLE recipes_to_images (
+  image_id INT NOT NULL,
+  review_id INT NOT NULL,
+  FOREIGN KEY (image_id) REFERENCES images (image_id),
+  -- FOREIGN KEY (image_id) REFERENCES images (image_id) ON DELETE CASCADE,
+  FOREIGN KEY (recipe_id) REFERENCES recipes (recipe_id)
+  -- FOREIGN KEY (recipe_id) REFERENCES recipes (recipe_id) ON DELETE CASCADE
+);
+
+CREATE TABLE favorites (
+  recipe_id INT NOT NULL,
+  image_id INT NOT NULL,
+  FOREIGN KEY (recipe_id) REFERENCES recipes (recipes_id),
+  -- FOREIGN KEY (recipe_id) REFERENCES recipes (recipes_id) ON DELETE CASCADE,
+  FOREIGN KEY (images_id) REFERENCES images (image_id)
+  -- FOREIGN KEY (images_id) REFERENCES images (image_id) ON DELETE CASCADE
 );
