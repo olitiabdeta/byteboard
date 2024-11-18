fe58b3e06cb306601460e7e54dfcf234e01cf87b
Kaylee Dehncke <kaylee.dehncke@colorado.edu>
2024-11-18 08:52:28 -0700

    changed index.js

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -12,6 +12,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcryptjs'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+const { error } = require('console');
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'src', 'resources')));
 app.use(express.static(__dirname + '/'));
@@ -94,20 +95,148 @@ app.use((req, res, next) => {
   next();
 });
 
-// Home page
-app.get('/home', (req, res) => {
-  res.render('pages/home');
+//GET login page 
+app.get('/login', (req, res) => {
+  res.render('pages/login');
 });
 
-// Register page
+//POST login
+app.post('/login', async (req,res) =>{
+  try
+  {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userQuery = 'SELECT * FROM users WHERE username = $1';
+    const userResult = await db.oneOrNone(userQuery, [username]);
+
+    if(userResult)
+    {
+      const user = userResult;
+
+      //compare the entered password with the hashed password from the database
+      const match = await bcrypt.compare(password, user.password);
+
+      if(match)
+      {
+        req.session.user = user;
+        req.session.save();
+        return res.redirect('/home');
+      }
+      else
+      {
+        return res.render('pages/login', {message: "Incorrect username or password.", error: true});
+      }
+    }
+    //if user is not found, redirect to register page
+    else
+    {
+      return res.redirect('/register');
+    }
+  }
+  catch(error)
+  {
+    console.error("Login error:", error);
+    res.render('pages/login', {message: "An error occured during login. Please try again.", error: true});
+  }
+});
+
+// POST route for login
+/*app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    
+    // Find the user in the database
+    const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', [username]);
+    console.log(user);
+    if (!user) 
+    {
+      // if not found
+      return res.render('pages/login', {message: "Incorrect username or password.", error: true});
+    }
+    
+    // Compare the provided password with the hashed password in the database
+    const match = await bcrypt.compare(password, user.password);
+    
+    
+    if (!match) 
+    {
+      //if not corect
+      return res.render('pages/login', { message: 'Incorrect username or password.' }); // /login ->pages/login
+    } 
+    
+    // Save user details
+    req.session.user = user;
+    req.session.save();
+    
+    
+    // redirect to the home route
+    return res.redirect('/home');
+    } 
+    catch (error) 
+    {
+      console.error('Login error:', error);
+      return res.render('pages/login', { message: 'An error occurred. Please try again.' });
+    }
+    });*/
+
+//GET Register page
 app.get('/register', (req, res) => {
   res.render('pages/register');
 });
 
-// Display login page 
-app.get('/login', (req, res) => {
-    res.render('pages/login');
-  });
+//POST Register
+app.post('/register', async (req, res) => {
+  //hash the password using bcrypt library
+  try
+  {
+    const username = req.body.username;
+    const password = req.body.password;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const email = req.body.email;
+    const first_name = req.body.firstname;
+    const last_name = req.body.lastname;
+    
+    // To-DO: Insert username and hashed password into the 'users' table
+    //let something = await db.any('INSERT INTO users (username, password, email, first_name, last_name) VALUES($1, $2, $3, $4, $5)', [username, hash, email, first_name, last_name]);
+    const query = `INSERT INTO users (username, password, email, first_name, last_name) VALUES($1, $2, $3, $4, $5)`;
+    const values = [username, hashedPassword, email, first_name, last_name];
+    
+    const registeredUser = await db.none(query, values);
+    
+    console.log(hashedPassword);
+    console.log(registeredUser);
+    if(registeredUser)
+    {
+      res.redirect('/login');
+    }
+  } 
+  
+  catch(error)
+  {
+    console.error("Error inserting user:", error);
+
+    //if insertion failsm reirect back to the register page
+    res.redirect('/register');
+  }
+});
+
+// Authentication Middleware.
+const auth = (req, res, next) => {
+  if (!req.session.user) 
+  {
+    // Default to login page.
+    return res.redirect('/login');
+  }
+  next();
+};
+
+app.use(auth);
+
+// Home page
+app.get('/home', (req, res) => {
+  res.render('pages/home');
+});
 
 //Friends
 app.get('/friends', (req, res) => {
@@ -121,98 +250,31 @@ app.get('/saved', (req, res) => {
 app.get('/createRecipe', (req, res) => {
   res.render('pages/createRecipe');
 });
-//Logout
-app.get('/Logout', (req,res)=> {
-  res.render('pages/logout')
-});
 
 
-// Register
-app.post('/register', async (req, res) => {
-  //hash the password using bcrypt library
-  try{
-  const username = req.body.username;
-  const hash = await bcrypt.hash(req.body.password, 10);
-  const email = req.body.email;
-  const first_name = req.body.firstname;
-  const last_name = req.body.lastname;
-  
-  
-  // To-DO: Insert username and hashed password into the 'users' table
-  let something = await db.any('INSERT INTO users (username, password, email, first_name, last_name) VALUES($1, $2, $3, $4, $5)', [username, hash, email, first_name, last_name]);
-    console.log(hash);
-  console.log(something);
-  if(something){
-  res.redirect('/login');
+// Logout route
+app.get('logout', (req,res) => {
+  try
+  {
+    req.session.destroy();
+    res.render('pages/logout');
   }
-  } catch{
-  res.redirect('/register');
+  catch(error)
+  {
+    console.error('Error logging out:', error);
   }
-  });
-  app.get('/login', (req, res) => {
-  res.render('pages/login'); // Render login.hbs, no initial message
-  });
-
-
-
-  // POST route for login
-  app.post('/login', async (req, res) => {
-    try {
-    const { username, password } = req.body;
-    
-    
-    // Find the user in the database
-    const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', [username]);
-    console.log(user);
-    if (!user) {
-    // if not found
-    return res.redirect('/register');
-    }
-    
-    
-    // Compare the provided password with the hashed password in the database
-    const match = await bcrypt.compare(password, user.password);
-    
-    
-    if (!match) {
-    //if not corect
-    return res.render('pages/login', { message: 'Incorrect username or password.' }); // /login ->pages/login
-    } 
-    
-    // Save user details
-    req.session.user = user;
-    req.session.save();
-    
-    
-    // redirect to the home route
-    return res.redirect('/home');
-    } catch (error) {
-    console.error('Login error:', error);
-    return res.render('pages/login', { message: 'An error occurred. Please try again.' });
-    }
-    });
-   
-    // Authentication Middleware.
-    const auth = (req, res, next) => {
-    if (!req.session.user) {
-    // Default to login page.
-    return res.redirect('/login');
-    }
-    next();
-    };
-
-
-  // Logout route
-  app.get('/logout', (req, res) => {
-    // Destroy the session
-    req.session.destroy((err) => {
-    if (err) {
-    console.error('Session destruction error:', err);
-    return res.redirect('/');
+})
+/*app.get('/logout', (req, res) => {
+  // Destroy the session
+  req.session.destroy((err) => {
+    if (err) 
+    {
+      console.error('Session destruction error:', err);
+      return res.redirect('/');
     }
     res.render('pages/logout', { message: 'Logged out Successfully' });
     });
-  });
+});*/
 
 
 // *****************************************************
