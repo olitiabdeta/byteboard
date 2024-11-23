b200f497b21d8ba8074217f3d4bcbc78393568b9
Oliti Abdeta <olab6122@colorado.edu>
2024-11-23 15:20:38 -0700

    testing to see if changes to section 2  will fix Render's disconnection to the right db

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -34,10 +34,11 @@ console.log(__dirname, path.join(__dirname, 'src', 'views'))
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(express.static(path.join(__dirname, 'resources'))); 
 
+require('dotenv').config(); 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
-  port: 5432, // the database port
+  host: process.env.POSTGRES_HOST || 'db', // the database server
+  port: process.env.POSTGRES_PORT || 5432, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
   password: process.env.POSTGRES_PASSWORD, // the password of the user account
