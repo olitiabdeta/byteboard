3541b31cf53a679695f71aeb4977a48ad8f0a6bc
Kaitlyn Rapp <kara8293@colorado.edu>
2024-12-03 15:24:15 -0700

    tryingtoaddfriends page

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,3 +1,8 @@
 /* Insert the values into corresponding columns in the table products. password=abcd*/
 INSERT INTO users (username, password,  email, first_name, last_name) VALUES
   ('suus', '$2a$10$LVgItd92Jw6FtDUCSg4yzOtOUUlepE/i5nevVNxrNRygZvX6i0ge6', 'sumeyyeustunel05@gmail.com', 'Sumeyye', 'Ustunel');
+
+INSERT INTO friends (user_id, friend_id, status)
+VALUES
+  ('suus', 'bob'),
+  ('alice', 'charlie');
